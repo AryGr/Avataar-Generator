93b436f0a04d084b125864da420857dcd217b3f5
Aryeh Grosskopf <aryeh2007@googlemail.com>
2022-01-21 23:23:49 +0000

    Bug-fix in enumFromString

--- lib/enums.dart
+++ lib/enums.dart
@@ -197,7 +197,6 @@ enum Graphic {
   skull
 }
 
-T enumFromString<T>(Iterable<T> values, String? value, T defaultValue) {
-  return values.firstWhere((type) => type.toString().split(".").last == value,
-      orElse: () => defaultValue);
+T enumFromString<T>(Iterable<T> values, String value, T defaultValue) {
+  return values.where((type) => type.toString() == value).first;
 }
